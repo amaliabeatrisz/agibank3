006da50bc84d032b3696855dfbc0e0684ed4f6ce
Pedro Paulo <87837439+pedropaulocg@users.noreply.github.com>
2022-03-18 11:15:33 -0300

    Add files via upload

---
+++ assets/js/faq.js
@@ -0,0 +1,49 @@
+var drop1 = document.querySelector('.question1');
+var drop2 = document.querySelector('.question2');
+var drop3 = document.querySelector('.question3');
+var drop4 = document.querySelector('.question4');
+var drop5 = document.querySelector('.question5');
+var drop6 = document.querySelector('.question6');
+var drop7 = document.querySelector('.question7');
+
+function faqDrop1(){
+
+    drop1.classList.toggle('active')
+
+}
+
+function faqDrop2(){
+
+    drop2.classList.toggle('active')
+
+}
+
+function faqDrop3(){
+
+    drop3.classList.toggle('active')
+
+}
+
+function faqDrop4(){
+
+    drop4.classList.toggle('active')
+
+}
+
+function faqDrop5(){
+
+    drop5.classList.toggle('active')
+
+}
+
+function faqDrop6(){
+
+    drop6.classList.toggle('active')
+
+}
+
+function faqDrop7(){
+
+    drop7.classList.toggle('active')
+
+}
